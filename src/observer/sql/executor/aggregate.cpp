8fc1577177d60d6ce3dd226718a58b0d4d082c56
liucc1997 <1192520566@qq.com>
2021-11-19 16:16:35 +0800

    aggregation-func-newfiles

---
+++ src/observer/sql/executor/aggregate.cpp
@@ -0,0 +1,148 @@
+#include "sql/executor/aggregate.h"
+// #include "common/log/log.h"
+#include "storage/common/date.h"
+
+
+Aggregate* Aggregate::aggregate_allocator(Table *table, AggregationType type, const char* attr_name) 
+{
+    const FieldMeta *field_meta = table->table_meta().field(attr_name);
+    if (type == AGCOUNT) {
+        if (field_meta == nullptr && 
+            strcmp(attr_name, "*") != 0 && strcmp(attr_name, "") != 0   //count(*) or count()
+        ) {
+            return nullptr;
+        }
+        return new CountAggregate(type, attr_name);
+    }
+    // 对于AVG MIN MAX 都需要指定attribute name
+    if (field_meta == nullptr) {
+        return nullptr;
+    } 
+    size_t feild_offset = field_meta->offset();
+    AttrType feild_type = field_meta->type();
+    if (type == AGMAX)  {
+        return new MaxAggregate(type, attr_name, feild_offset, feild_type);
+    }
+    if (type == AGMIN)  {
+        return new MinAggregate(type, attr_name, feild_offset, feild_type);
+    }
+    if (type == AGAVG && (feild_type == INTS || feild_type == FLOATS) ) {
+        return new AvgAggregate(type, attr_name, feild_offset, feild_type);
+    }
+    return nullptr;
+}
+
+void MaxAggregate::do_record(const char *record) {
+    if (!value_data_) {
+        value_data_ = record + feild_offset_;
+    }
+    int compare_ret = 0;
+    if (feild_type_ == INTS || feild_type_ == DATES) {
+        int v = *((int*)value_data_);
+        int record_v = *((int*)(record + feild_offset_));
+        if (record_v > v) {
+            value_data_ = record + feild_offset_;
+        }
+    }
+    if (feild_type_ == FLOATS ) {
+        float v = *((float*)value_data_);
+        float record_v = *((float*)(record + feild_offset_));
+        if (record_v > v) {
+            value_data_ = record + feild_offset_;
+        }
+    }
+    if (feild_type_ == CHARS ) {
+        const char* v = value_data_;
+        const char* record_v = record + feild_offset_;
+        if (strcmp(record_v, v) > 0) {
+            value_data_ = record + feild_offset_;
+        }
+    }
+}
+
+void MinAggregate::do_record(const char* record) {
+    if (!value_data_) {
+        value_data_ = record + feild_offset_;
+    }
+    int compare_ret = 0;
+    if (feild_type_ == INTS || feild_type_ == DATES) {
+        int v = *((int*)value_data_);
+        int record_v = *((int*)(record + feild_offset_));
+        if (record_v < v) {
+            value_data_ = record + feild_offset_;
+        }
+    }
+    if (feild_type_ == FLOATS ) {
+        float v = *((float*)value_data_);
+        float record_v = *((float*)(record + feild_offset_));
+        if (record_v < v) {
+            value_data_ = record + feild_offset_;
+        }
+    }
+    if (feild_type_ == CHARS ) {
+        const char* v = value_data_;
+        const char* record_v = record + feild_offset_;
+        if (strcmp(record_v, v) < 0) {
+            value_data_ = record + feild_offset_;
+        }
+    }
+}
+
+void AvgAggregate::do_record(const char *record) {
+    if (feild_type_ == INTS) {
+        int v = *((int*)(record + feild_offset_));
+        avg_ = avg_ * (count_*1.0/(count_+1)) + v * 1.0 / (count_ + 1);
+        count_ ++;
+    }
+    if (feild_type_ == FLOATS) {
+        float v = *((float*)(record + feild_offset_));
+        avg_ = avg_ * (count_*1.0/(count_+1)) + v / (count_ + 1);
+        count_ ++;
+    }
+}
+
+void CompositeAggregate::do_record(const char *record) {
+    for (std::vector<Aggregate*>::iterator iter = aggres_.begin(); iter != aggres_.end(); iter++) {
+        (*iter)->do_record(record);
+    }
+}
+void CompositeAggregate::set_tuple(TupleSet &tuple_set) {
+    Tuple tuple;
+    for (Aggregate* a : aggres_) {
+        if (a->get_result_poniter() == nullptr) {
+            printf("Failed to get result from Aggregate. %s", a->get_schema_attr());
+            return;
+        }
+        const char *p = a->get_result_poniter();
+        if (a->get_aggr_type() == AGCOUNT) {
+            tuple.add(*(int*)p);
+        }
+        if (a->get_aggr_type() == AGAVG) {
+            double t = *(double*)p;
+            tuple.add((float)t);
+        }        
+        if (a->get_aggr_type() == AGMAX || a->get_aggr_type() == AGMIN) {
+            if (a->get_attr_type() == INTS) {
+                tuple.add(*(int*)p);
+            }
+            if (a->get_attr_type() == FLOATS) {
+                tuple.add(*(float*)p);
+            }
+           if (a->get_attr_type() == CHARS) {
+                tuple.add(p, strlen(p));
+            }     
+            if (a->get_attr_type() == DATES) {
+                int t = *(int*)p;
+                Date d(t);
+                tuple.add(d.to_string(), DATA_STRING_LEN);
+            }
+        }
+    }
+    tuple_set.add(std::move(tuple));
+}
+
+CompositeAggregate::~CompositeAggregate () {
+    for (Aggregate* a : aggres_) {
+        delete a;
+    }
+}
---
+++ src/observer/sql/executor/aggregate.h
@@ -0,0 +1,111 @@
+#include <vector>
+#include "storage/common/table_meta.h"
+#include "storage/common/table.h"
+#include "sql/executor/tuple.h"
+
+class Aggregate {
+    public:
+    static void record_reader(const char *data, void *context) {
+        Aggregate *converter = (Aggregate *)context;
+        converter->do_record(data);
+    }
+    Aggregate(AggregationType type, const char* attr_name) : aggr_type_(type), attr_name_(attr_name), feild_type_(UNDEFINED) {}
+    virtual void do_record(const char *record) = 0;
+    virtual const char* get_result_poniter() {
+        return nullptr;
+    }    
+    static Aggregate* aggregate_allocator(Table *table, AggregationType type, const char* attr_name);
+    virtual const char* get_schema_attr() { return schema_attr_;}
+    AttrType get_attr_type() { return feild_type_;}
+    AggregationType get_aggr_type() { return aggr_type_; }
+    protected:
+    char schema_attr_[16];
+    size_t feild_offset_;
+    AttrType feild_type_;
+    AggregationType aggr_type_;
+    const char* attr_name_;
+};
+
+class CountAggregate : public Aggregate {
+    private:
+    int count_;
+    public:
+    CountAggregate(AggregationType type, const char* attr_name) : count_(0), Aggregate(type, attr_name) {}
+    void do_record(const char *record) {
+        count_ ++;
+    }
+    virtual const char* get_result_poniter() {
+        return (char*)(&count_);
+    }
+    virtual const char* get_schema_attr() { 
+        snprintf(schema_attr_, 16, "count(%s)", attr_name_);
+        return schema_attr_;
+    }
+};
+
+class MaxAggregate : public Aggregate {
+    public:
+    MaxAggregate(AggregationType type, const char* attr_name, size_t feild_offset, AttrType feild_type) : 
+        Aggregate(type, attr_name), value_data_(nullptr)
+    {
+        feild_offset_ = feild_offset;
+        feild_type_ = feild_type;
+    }
+    void do_record(const char *record);
+    virtual const char* get_result_poniter() {
+        return value_data_;
+    }
+    virtual const char* get_schema_attr() { 
+        snprintf(schema_attr_, 16, "max(%s)", attr_name_);
+        return schema_attr_;
+    }
+    protected:
+    const char* value_data_;
+};
+
+class MinAggregate : public MaxAggregate {
+    public:
+    MinAggregate(AggregationType type, const char* attr_name, size_t feild_offset, AttrType feild_type) : 
+        MaxAggregate(type, attr_name, feild_offset, feild_type) {}
+    void do_record(const char *record);
+    virtual const char* get_schema_attr() { 
+        snprintf(schema_attr_, 16, "min(%s)", attr_name_);
+        return schema_attr_;
+    }
+};
+
+class AvgAggregate : public Aggregate {
+    public:
+    AvgAggregate(AggregationType type, const char* attr_name, size_t feild_offset, AttrType feild_type) : 
+        Aggregate(type, attr_name), avg_(0), count_(0) 
+    {
+        feild_offset_ = feild_offset;
+        feild_type_ = feild_type;
+    }
+    void do_record(const char *record);
+    virtual const char* get_result_poniter() {
+        return (char*)(&avg_);
+    }
+    virtual const char* get_schema_attr() { 
+        snprintf(schema_attr_, 16, "avg(%s)", attr_name_);
+        return schema_attr_;
+    }
+    private:
+    double avg_;
+    size_t count_;
+};
+
+class CompositeAggregate : public Aggregate {
+    public:
+    void add_aggration(Aggregate* a) {
+        aggres_.push_back(a);
+    }
+    CompositeAggregate () : Aggregate(UNKNOW_AGTYPE, nullptr) {}
+    ~CompositeAggregate ();
+    std::vector<Aggregate*>& get_aggre_list() { return aggres_; }
+    void do_record(const char *record);
+    virtual char* get_schema_attr() { return nullptr; }
+    void set_tuple(TupleSet &tuple_set);
+    private:
+    std::vector<Aggregate*> aggres_;
+};
